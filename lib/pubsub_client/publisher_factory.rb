b7c5c08ce0edd80aca9076b594c2ecf57460fe72
Simon Sanchez <ssanchez@apartmentlist.com>
2020-08-14 16:55:22 -0700

    WIP: Add mutex

--- lib/pubsub_client/publisher_factory.rb
+++ lib/pubsub_client/publisher_factory.rb
@@ -6,19 +6,27 @@ module PubsubClient
   class PublisherFactory
     def initialize(topic_name)
       @topic_name = topic_name
+      @mutex = Mutex.new
     end
 
     def build
       return @publisher if @publisher_pid == current_pid
-      pubsub = Google::Cloud::PubSub.new
-      topic = pubsub.topic(topic_name)
-      @publisher_pid = Process.pid
-      @publisher = Publisher.new(topic)
+
+      mutex.synchronize do
+        unless @publisher_pid == current_pid
+          pubsub = Google::Cloud::PubSub.new
+          topic = pubsub.topic(topic_name)
+          @publisher_pid = Process.pid
+          @publisher = Publisher.new(topic)
+        end
+      end
+
+      @publisher
     end
 
     private
 
-    attr_reader :topic_name
+    attr_reader :mutex, :topic_name
 
     # Used for testing to simulate when a
     # process is forked. In those cases,
