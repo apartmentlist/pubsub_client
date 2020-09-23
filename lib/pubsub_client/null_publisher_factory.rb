74117ba28e124f0d1c6cb59fb40f2115ae3e7ae6
Simon Sanchez <ssanchez@apartmentlist.com>
2020-09-23 03:01:05 -0700

    Introduce `NullPublishersHash` to mimic real publishers hash
    The `PublisherFactory.build` method now returns a hash where the
    key is the topic name and the value is the `Publisher` object.
    This new null class mimics this hash.

--- lib/pubsub_client/null_publisher_factory.rb
+++ lib/pubsub_client/null_publisher_factory.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'null_publisher'
+require_relative 'null_publishers_hash'
 
 module PubsubClient
   # A null object to act as a publisher factory when clients are in dev or test
   class NullPublisherFactory
     def build
-      NullPublisher.new
+      NullPublishersHash.new
     end
   end
 end
