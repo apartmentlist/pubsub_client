3cdc576042c97c5c622291efa69ded67c686ec56
Simon Sanchez <ssanchez@apartmentlist.com>
2020-10-20 03:05:21 -0700

    `NullSubscriber`: yield string message

--- lib/pubsub_client/null_subscriber.rb
+++ lib/pubsub_client/null_subscriber.rb
@@ -3,12 +3,8 @@
 module PubsubClient
   # A null object to act as a subscriber when clients are in dev or test
   class NullSubscriber
-    # This is required so that this publisher maintains the same contract
-    # as a real publisher.
-    NullResult = Struct.new(:data, :acknowledge!)
-
     def subscribe(*, &block)
-      yield NullResult.new
+      yield 'message'
     end
   end
 end
