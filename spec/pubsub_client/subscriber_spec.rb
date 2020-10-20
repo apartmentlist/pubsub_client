242a80ff5d94389eaf4b7a68309ba227a2d8e626
Simon Sanchez <ssanchez@apartmentlist.com>
2020-10-20 12:38:55 -0700

    Specs for listener block

--- spec/pubsub_client/subscriber_spec.rb
+++ spec/pubsub_client/subscriber_spec.rb
@@ -27,7 +27,8 @@ module PubsubClient
         allow(subscriber).to receive(:sleep)
           .and_raise(SignalException.new('HUP'))
 
-        allow(listener).to receive(:stop) { listener }
+        allow(listener).to receive(:stop)
+          .and_return(listener)
         allow(listener).to receive(:wait!)
       end
 
@@ -35,6 +36,39 @@ module PubsubClient
         subject.subscribe(1, true)
         expect(listener).to have_received(:stop)
         expect(listener).to have_received(:wait!)
+      end
+    end
+
+    context 'listener block' do
+      let(:pubsub_message) {
+        instance_double(Google::Cloud::PubSub::ReceivedMessage, data: 'the-message', acknowledge!: nil)
+      }
+
+      before do
+        allow(subscription).to receive(:listen)
+          .with({ threads: { callback: 1 } })
+          .and_yield(pubsub_message)
+          .and_return(listener)
+      end
+
+      it 'yields the message' do
+        yielded_message = nil
+        subject.subscribe(1, true) do |message|
+          yielded_message = message
+        end
+        expect(yielded_message).to eq('the-message')
+      end
+
+      it 'acks the message' do
+        subject.subscribe(1, true) { |_| }
+        expect(pubsub_message).to have_received(:acknowledge!)
+      end
+
+      context 'when auto ack is not desired' do
+        it 'does not ack the message' do
+          subject.subscribe(1, false) { |_| }
+          expect(pubsub_message).to_not have_received(:acknowledge!)
+        end
       end
     end
   end
